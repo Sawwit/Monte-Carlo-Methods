215b70d4fa7c6ca66d3c14a555bcfb77a8ba9efd
Dirrenvv99 <58955723+Dirrenvv99@users.noreply.github.com>
2021-12-19 00:03:16 +0100

    Creation/update of finetuners

--- HMC_finetuning.py
+++ HMC_finetuning.py
@@ -51,7 +51,7 @@ def I_pi(phi,pi,lamb,kappa,eps):
 def I_phi(phi,pi,eps):
     return phi + eps*pi
 
-def leapfrog(phi,pi,eps,tau,kappa):
+def leapfrog(phi,pi,eps,tau,kappa, lamb):
     a=phi
     b=pi
 
@@ -67,7 +67,7 @@ def scalar_HMC_step(phi,lamb,kappa,eps,tau):
     #Sample pi as random (normally distributed) noise for every lattice site
     width = phi.shape[0]
     pi = np.random.multivariate_normal(np.array([0 for _ in range(width)]),np.eye(width),(width,width,width))  
-    phi_new, pi_new, phi_old, pi_old = leapfrog(phi,pi,eps,tau,kappa)
+    phi_new, pi_new, phi_old, pi_old = leapfrog(phi,pi,eps,tau,kappa, lamb)
     delta_H = scalar_ham_diff(phi_old,pi_old,phi_new,pi_new,lamb,kappa)
     if delta_H <0 or rng.uniform() <np.exp(-delta_H):
         return 1, phi_new
@@ -101,72 +101,166 @@ parser.add_argument('-iw',type= int, help='lowest lattice size tested')
 parser.add_argument('-hw',type= int, help='Highest lattice size tested')
 parser.add_argument('-ki',type=float, help='Starting value for Kappa')
 parser.add_argument('-kf',type=float, help='End value for Kappa')
-parser.add_argument('-ka',type=int, default = 11, help='Amount of Kappas')
-parser.add_argument('-l', type=float, default = 1.5, help ='lambda')
+parser.add_argument('-li', type = float, help = 'Lowest value of lambda')
+parser.add_argument('-lf', type = float, help = 'highest value of lambda')
+parser.add_argument('-la', type = int, help = 'amount of lambdas')
 parser.add_argument('-s', type = int, default = 300, help = "number of sweeps to finetune")
-parser.add_argument('-e', type=int, default=1000, help='Number E of equilibration sweeps')
-parser.add_argument('-m', type=int, default=800, help='Number M of sweeps per measurement')
-parser.add_argument('-o', type=int, default=30, help='Time in seconds between file outputs')
-parser.add_argument('-ep', type=float, default = 0.15, help = 'Stepsize of leapfrog integrator')
-parser.add_argument('-tau',type=int,default=10,help='discretization steps of leapfrog integrator')
+parser.add_argument('-f', help='Output filename')
 
 args = parser.parse_args()
 #Some Sanity checks
 if args.hw is None or args.hw < 1:
     parser.error("Please specify a positive lattice size!")
-if args.l is None or args.l <= 0.0:
-    parser.error("Please specify a positive lambda!")
-if args.e < 10:
-    parser.error("Need at least 10 equilibration sweeps")
-if args.tau*args.ep >=1.5 or args.tau*args.ep <=0.5:
-    print("The product of epsilon and tau is not close to the desired value of 1.")
+if args.f is None:
+    # construct a filename from the parameters plus a timestamp (to avoid overwriting)
+    output_filename = "HMC_finetuning_output_wi{}_wf{}_li{}_lf{}_{}.json".format(args.iw,args.hw,int(10 *args.li), int(10 * args.lf),time.strftime("%Y%m%d%H%M%S"))
+else:
+    output_filename = args.f
 
-kappas = np.linspace(args.ki,args.kf,args.ka)
-lamb = args.l
-equil_sweeps = args.e
-measurements = args.m
+lambdas= np.linspace(args.li, args.lf, args.la)
 
 
-def low_k_checker(width,lamb,kappa_low,eps,tau,sweeps):
+def other_k_checker(width,lamb,kappa_low,eps,tau,sweeps):
     phi_state_low = np.zeros((width,width,width,width))
     acceptions, phi_state_low = run_scalar_MH(phi_state_low,lamb,kappa_low,eps,tau,sweeps)
     acc_rate = acceptions/sweeps
+    print(acc_rate, kappa_low)
     if acc_rate > 0.15 and acc_rate < 0.9:
-        return True
+        return acc_rate,  True
     else:
-        return False
+        return acc_rate,  False
 
 
 def main():
     fine_tuned_values = []
+    start_time = time.time()
     for width in range(args.iw,args.hw+1):
-        print("\n Finetuning process for width: ", width)
-        for index, (eps,tau) in enumerate(eps_taus):
-            print('\n', index, " Finetuning step")
-            phi_state_highk = np.zeros((width,width,width,width))
-            kappa_high = args.kf
-            sweeps = args.s
-            acceptions, phi_state_highk = run_scalar_MH(phi_state_highk,lamb,kappa_high,eps,tau,sweeps)
-            acc_rate = acceptions/sweeps
-            if acc_rate > 0.15:
-                if acc_rate < 0.9:
-                    if low_k_checker(width,lamb, args.ki, eps, tau, sweeps):
-                        fine_tuned_values.append((width,eps,tau))
-                        print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width)
-                        break
-                    else: 
-                        if index != 0:
-                            eps, tau = eps_taus[index -1]
-                            fine_tuned_values.append((width,eps,tau))
-                            print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width)
-                            break
-            elif index == len(eps_taus) - 1:
-                fine_tuned_values.append((width,eps,tau))
-                print("lowest value eps was not sufficient, but is still added")
-                print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width)
-        
+        for lambd in lambdas:
+            print("\n Finetuning process for width: ", width, "and Lambda: ", lambd)
+            high_has_higher_acc = False
+            for index, (eps,tau) in enumerate(eps_taus):
+                if high_has_higher_acc == False:                    
+                    tau = int(tau)
+                    print('\n', index, " Finetuning step")
+                    phi_state_highk = np.zeros((width,width,width,width))
+                    kappa_high = args.kf
+                    sweeps = args.s
+                    acceptions = 0
+                    acceptions, phi_state_highk = run_scalar_MH(phi_state_highk,lambd,kappa_high,eps,tau,sweeps)
+                    acc_rate = acceptions/sweeps
+                    print(acc_rate, kappa_high)
+                    if acc_rate > 0.15:
+                        if acc_rate < 0.9:
+                            other_acc, checker = other_k_checker(width,lambd, args.ki, eps, tau, sweeps)
+                            if other_acc < acc_rate:
+                                high_has_higher_acc = True
+                            if checker:
+                                fine_tuned_values.append((width,lambd,eps,tau))
+                                print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                                break
+                            else:
+                                if other_acc > 0.9 and acc_rate > 0.75 and high_has_higher_acc == False:
+                                    eps = eps_taus[index][0] + 0.005
+                                    tau = int(round(1/eps))
+                                    print("step of epsilon was too big -> eps + 0.005 added")
+                                    print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                                    fine_tuned_values.append((width,lambd,eps,tau))
+                                    break  
+                                if acc_rate > 0.9 and other_acc > 0.75 and high_has_higher_acc == True:
+                                    eps = eps_taus[index][0] + 0.005
+                                    tau = int(round(1/eps))
+                                    print("step of epsilon was too big -> eps + 0.005 added")
+                                    print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                                    fine_tuned_values.append((width,lambd,eps,tau))
+                                    break       
 
-        
+                            # else:  (COMMENT: voor nu kies ik ervoor dit weg te halen, en daarmee te eisen dat allebei de kappas altijd moeten gelden, door het printen van bijbehorende )
+                            #     # if index != 0:
+                            #     #     eps, tau = eps_taus[index -1]
+                            #     #     fine_tuned_values.append((width,lambd,eps,tau))
+                            #     #     print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                            #     #     break
+                        else:
+                            if index == len(eps_taus) - 1:
+                                fine_tuned_values.append((width,lambd,eps,tau))
+                                print("lowest value eps was not sufficient, but is still added")
+                                print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+
+                            if index == 0:
+                                acc_other, _ = other_k_checker(width,lambd, args.ki, eps, tau, sweeps)
+                                if acc_other > 0.9:                                    
+                                    eps = eps_taus[0][0] + 0.01
+                                    tau = int(round(1/eps))
+                                    print("First value had a too low epsilon -> eps + 0.01 added")
+                                    print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                                    fine_tuned_values.append((width,lambd,eps,tau))
+                                    break                            
+                        
+                    elif index == len(eps_taus) - 1:
+                        fine_tuned_values.append((width,lambd,eps,tau))
+                        print("lowest value eps was not sufficient, but is still added")
+                        print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                else:                  
+                    tau = int(tau)
+                    print('\n', index, " Finetuning step")
+                    phi_state_lowk = np.zeros((width,width,width,width))
+                    kappa_low = args.ki
+                    sweeps = args.s
+                    acceptions = 0
+                    acceptions, phi_state_lowk = run_scalar_MH(phi_state_lowk,lambd,kappa_low,eps,tau,sweeps)
+                    acc_rate = acceptions/sweeps
+                    print(acc_rate, kappa_low)
+                    if acc_rate > 0.15:
+                        if acc_rate < 0.9:                  
+                            other_acc, checker = other_k_checker(width,lambd, args.kf, eps, tau, sweeps)
+                            if other_acc < acc_rate:
+                                high_has_higher_acc = False
+                            if checker:
+                                fine_tuned_values.append((width,lambd,eps,tau))
+                                print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                                break
+                            else:
+                                if other_acc > 0.9 and acc_rate > 0.75 and high_has_higher_acc == True:
+                                    eps = eps_taus[index][0] + 0.005
+                                    tau = int(round(1/eps))
+                                    print("step of epsilon was too big -> eps + 0.005 added")
+                                    print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                                    fine_tuned_values.append((width,lambd,eps,tau))
+                                    break  
+                                elif acc_rate > 0.9 and other_acc > 0.75 and high_has_higher_acc == False:
+                                    eps = eps_taus[index][0] + 0.005
+                                    tau = int(round(1/eps))
+                                    print("step of epsilon was too big -> eps + 0.005 added")
+                                    print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                                    fine_tuned_values.append((width,lambd,eps,tau))
+                                    break 
+                            # else:  (COMMENT: voor nu kies ik ervoor dit weg te halen, en daarmee te eisen dat allebei de kappas altijd moeten gelden, door het printen van bijbehorende )
+                            #     # if index != 0:
+                            #     #     eps, tau = eps_taus[index -1]
+                            #     #     fine_tuned_values.append((width,lambd,eps,tau))
+                            #     #     print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                            #     #     break
+                        elif index == len(eps_taus) - 1:
+                            fine_tuned_values.append((width,lambd,eps,tau))
+                            print("lowest value eps was not sufficient, but is still added")
+                            print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+                    elif index == len(eps_taus) - 1:
+                        fine_tuned_values.append((width,lambd,eps,tau))
+                        print("lowest value eps was not sufficient, but is still added")
+                        print("\n values found; eps: " ,eps, " tau : ", tau, ". For width: ", width, "and Lambda: ", lambd)
+            current_time = time.time()
+            run_time = int(current_time - start_time)
+            with open(output_filename,'w') as outfile:
+                json.dump({ 
+                'parameters': vars(args),
+                'start_time': time.asctime(time.localtime(start_time)),
+                'current_time': time.asctime(time.localtime(current_time)),
+                'run_time_in_seconds': run_time,
+                'current_width': fine_tuned_values[-1][0],
+                'current_lambda': fine_tuned_values[-1][1],
+                'fine_tuned_values': fine_tuned_values
+                }, outfile, cls=NumpyEncoder)
+
     print(fine_tuned_values)
 
 
--- heatbath_finetuner.py
+++ heatbath_finetuner.py
@@ -2,6 +2,20 @@ import numpy as np
 rng = np.random.default_rng()  
 import matplotlib.pylab as plt
 import argparse
+import time
+import json
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
 
 def potential_v(x,lamb):
     '''Compute the potential function V(x).'''
@@ -49,16 +63,28 @@ parser.add_argument('-iw',type= int, help='lowest lattice size tested')
 parser.add_argument('-hw',type= int, help='Highest lattice size tested')
 parser.add_argument('-ki',type=float, help='Starting value for Kappa')
 parser.add_argument('-kf',type=float, help='End value for Kappa')
+parser.add_argument('-li', type = float, help = 'Lowest value of lambda')
+parser.add_argument('-lf', type = float, help = 'highest value of lambda')
+parser.add_argument('-la', type = int, help = 'amount of lambdas')
 parser.add_argument('-s', type = int, default = 300, help = "number of sweeps to finetune")
 parser.add_argument('-lowa', type = float, default = 0.4, help = "lowest acceptance rate accepted")
 parser.add_argument('-higha', type = float, default = 0.6, help = "highest acceptance rate accepted")
+parser.add_argument('-f', help='Output filename')
 args = parser.parse_args()
+
+if args.f is None:
+    # construct a filename from the parameters plus a timestamp (to avoid overwriting)
+    output_filename = "heatbath_finetuning_output_wi{}_wf{}_li{}_lf{}_{}.json".format(args.iw,args.hw,int(10 *args.li), int(10 * args.lf),time.strftime("%Y%m%d%H%M%S"))
+else:
+    output_filename = args.f
+
 if args.hw is None or args.hw < 1:
     parser.error("Please specify a positive lattice size!")
 
 deltas = np.arange(2.5,0.09,-0.2)
 deltas = np.array([round(delta,2) for delta in deltas])
-lamb = 1.5
+lambdas = np.linspace(args.li, args.lf, args.la)
+
 
 def low_k_checker(width,lamb, kappa, delta, sweeps, num_sites):
     phi_state_low = np.random.multivariate_normal(np.array([0 for _ in range(width)]),np.eye(width),(width,width,width))
@@ -71,44 +97,60 @@ def low_k_checker(width,lamb, kappa, delta, sweeps, num_sites):
 
 def main():
     fine_tuned_values = []
+    start_time = time.time()
     for width in range(args.iw,args.hw+1):
-        num_sites = width**4
-        print("\n Finetuning process for width: ", width)
-        for index, delta in enumerate(deltas):
-            phi_state_highk = np.random.multivariate_normal(np.array([0 for _ in range(width)]),np.eye(width),(width,width,width))
-            kappa_high = args.kf
-            sweeps = args.s
-            total_accept = run_scalar_MH(phi_state_highk,lamb,kappa_high,delta,sweeps * num_sites)
-            acc_rate = total_accept/(sweeps * num_sites)
-            print(acc_rate)
-            if acc_rate > args.lowa:
-                if acc_rate < args.higha:
-                    if low_k_checker(width,lamb, args.ki, delta, sweeps, num_sites):
-                         fine_tuned_values.append((width, delta))
-                         print("\n values found; delta: ", delta, " For width: ", width)
-                         break
+        for lamb in lambdas:
+            num_sites = width**4
+            print("\n Finetuning process for width: ", width, "and Lambda: ", lamb)
+            for index, delta in enumerate(deltas):
+                phi_state_highk = np.random.multivariate_normal(np.array([0 for _ in range(width)]),np.eye(width),(width,width,width))
+                kappa_high = args.kf
+                sweeps = args.s
+                total_accept = run_scalar_MH(phi_state_highk,lamb,kappa_high,delta,sweeps * num_sites)
+                acc_rate = total_accept/(sweeps * num_sites)
+                print(acc_rate)
+                if acc_rate > args.lowa:
+                    if acc_rate < args.higha:
+                        if low_k_checker(width,lamb, args.ki, delta, sweeps, num_sites):
+                            fine_tuned_values.append((width, lamb, delta))
+                            print("\n values found; delta: ", delta, " For width: ", width)
+                            break
+                        else:
+                            if index != 0:
+                                delta = deltas[index - 1]
+                            fine_tuned_values.append((width, lamb, delta))
+                            print("\n values found; delta: ", delta, " For width: ", width)
+                            break
                     else:
-                        if index != 0:
-                            delta = deltas[index - 1]
-                        fine_tuned_values.append((width, delta))
-                        print("\n values found; delta: ", delta, " For width: ", width)
-                        break
-                else:
-                    delta = delta + 0.1
-                    if low_k_checker(width,lamb, args.ki, delta, sweeps, num_sites):
-                         fine_tuned_values.append((width, delta))
-                         print("\n values found; delta: ", delta, " For width: ", width)
-                         break
-                    else:
-                        delta = delta - 0.05
-                        fine_tuned_values.append((width, delta))
-                        print("\n values found; delta: ", delta, " For width: ", width)
-                        break
-            elif index == len(deltas) - 1:
-                fine_tuned_values.append((width, delta))
-                print("\n values found; delta: ", delta, " For width: ", width)
-                print("lowest value delta was not sufficient, but is still added, although it was with a too low acceptance rate of: ", acc_rate)
-    print(fine_tuned_values)
+                        delta = delta + 0.1
+                        if low_k_checker(width,lamb, args.ki, delta, sweeps, num_sites):
+                            fine_tuned_values.append((width, lamb, delta))
+                            print("\n values found; delta: ", delta, " For width: ", width)
+                            break
+                        else:
+                            delta = delta - 0.05
+                            fine_tuned_values.append((width, lamb, delta))
+                            print("\n values found; delta: ", delta, " For width: ", width)
+                            break
+                elif index == len(deltas) - 1:
+                    fine_tuned_values.append((width, lamb, delta))
+                    print("\n values found; delta: ", delta, " For width: ", width)
+                    print("lowest value delta was not sufficient, but is still added, although it was with a too low acceptance rate of: ", acc_rate)
+                current_time = time.time()
+                run_time = int(current_time - start_time)
+            with open(output_filename,'w') as outfile:
+                json.dump({ 
+                    'parameters': vars(args),
+                    'start_time': time.asctime(time.localtime(start_time)),
+                    'current_time': time.asctime(time.localtime(current_time)),
+                    'run_time_in_seconds': run_time,
+                    'current_width': fine_tuned_values[-1][0],
+                    'current_lambda': fine_tuned_values[-1][1],
+                    'fine_tuned_values': fine_tuned_values
+                    }, outfile, cls=NumpyEncoder)
+    print("If this message is displayed, the JSON file should be filled as needed")
+
+
 
 if __name__ == '__main__':
     main()
