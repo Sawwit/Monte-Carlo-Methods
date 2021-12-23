55ab8165125c25cc966313e55e2e435ae07ebb03
Dirrenvv99 <58955723+Dirrenvv99@users.noreply.github.com>
2021-12-23 14:23:30 +0100

    All data from the server calculations + restructure of folders

--- fine_tune_test_job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=hefstud
-#SBATCH --output=std_%A_%a.txt
-#SBATCH --mem=100M
-#SBATCH --time=3-0:00:00
-cd ~/monte-carlo-techniques-2021/Project
-/software/anaconda3/bin/python3 HMC_finetuning.py -iw 10 -hw 10 -ki 0.08 -kf 0.18 -li 1.5 -lf 2 -la 2
